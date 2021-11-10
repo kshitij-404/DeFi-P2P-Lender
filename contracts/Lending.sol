cff47ed1252947be9c62f20b8d9280e90a7fc04c
Rishabh <rishabhkr.073@gmail.com>
2021-11-10 17:06:23 +0530

    Adding commits

--- contracts/Lending.sol
+++ contracts/Lending.sol
@@ -1,7 +1,6 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Lending {
-
     // borrower
 
     struct Proposal {
@@ -15,10 +14,13 @@ contract Lending {
 
     mapping(uint256 => address) public proposalToBorrower;
 
-    function createProposal(uint256 _loanAmount, uint256 _time, bytes32 _mortgage) public {
+    function createProposal(
+        uint256 _loanAmount,
+        uint256 _time,
+        bytes32 _mortgage
+    ) public {
         proposals.push(Proposal(msg.sender, _loanAmount, _time, _mortgage));
         uint256 _proposalId = proposals.length - 1;
         proposalToBorrower[_proposalId] = msg.sender;
     }
-
 }
