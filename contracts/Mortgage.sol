1b3693fc6f2ccd18471e9740379d05ce82919a2b
shreshthgoyal <shreshthg30@gmail.com>
2021-11-12 18:20:05 +0530

    contracts

--- contracts/Mortgage.sol
+++ contracts/Mortgage.sol
@@ -4,29 +4,15 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Mortgage {
   address public borrower;
 
-  mapping (bytes32=>address) public mortgageToBorrower;
-  mapping (address=>bytes32[]) public borrowerToMortgage;
+  mapping (string=>address) public mortgageToBorrower;
+  mapping (address=>string) public borrowerToMortgage;
 
   constructor() public {
       borrower = msg.sender;
   }
   
-  function addMortgage(bytes32 document) public {
-
+  function addMortgage(string memory document) public {
     mortgageToBorrower[document] = msg.sender;
-
-    uint count = borrowerToMortgage[msg.sender].length;
-
-    uint i;
-    for(i=0;i<count; i++)
-    {
-      if(borrowerToMortgage[msg.sender][i] == document)
-        return;
-    }
-    borrowerToMortgage[msg.sender].push(document);
-  }
-
-  function getCountMortgage(address person) public view returns(uint) {
-    return borrowerToMortgage[person].length;
+    borrowerToMortgage[msg.sender] = document;
   }
 }
